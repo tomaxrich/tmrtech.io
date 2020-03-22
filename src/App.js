b31ac062e4c162a7ed6807701a9b1afd3a0a7ade
Thomas M R <34826076+tomaxrich@users.noreply.github.com>
2020-03-21 19:10:06 -0600

    first attempt at getting react to work
    first commit to deploy react app in Github.io

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Currently Under Development
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+        /*
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          color: #61dafb;
+          */
+        >
+          
+        </a>
+        <p className="tech">
+          Currently Implemented Technology
+        </p>
+        <p className="tech">Github.io</p>
+        <p className="tech">React.js</p>
+        <p className="tech">Node.js</p>
+        <p className="tech">GoDaddy.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {   
+    return (
+        <div className='box'>
+        <nav >
+            <div className="container">
+                    <a className="logo">TMR Technology</a>
+            </div>
+        </nav>
+        </div>
+    );
+}
+
+
+export default Navbar
